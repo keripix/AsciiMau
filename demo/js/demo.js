68332e9cb8f62ab560417eba8728febf4b5f6b7e
A. Akbar Hidayat <keripix@gmail.com>
2013-09-07 12:31:46 +0700

    event driven

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -3,11 +3,14 @@
         fontPath: "./../fonts"
       }),
       fontList = global.fontList,
-      $fontlistEl = $("#font-list");
+      $fontlistEl = $("#font-list"),
+      $tulisan = $("#tulisan"),
+      currentFont = undefined;
 
   /**
    * Memuat fonts
    */
+  
   fontList.forEach(function(key){
     key = key.charAt(0).toUpperCase() + key.substr(1);
     $fontlistEl.append("<option value='" + key + "'>" + key + "</option>")
@@ -16,15 +19,50 @@
   /**
    * Event handler yang akan kita gunakan
    */
-  function handleLoadedFont(content){
-    console.log(content);
+  
+  $(document).on("fontUpdated", function(e, data){
+
+  });
+
+  $(document).on("fontLoaded", function(e, data){
+    currentFont = data.font;
+  });
+
+  $(document).on("writtenTextUpdated", function(e, data){
+
+  });
+
+  $(document).on("asciiArtUpdated", function(e, data){
+    if (data.asciiArt) {
+      $("#asciiArt").html("<pre>" + data.asciiArt + "</pre>")
+    }
+  });
+  
+  function handleLoadedFont(font){
+    renderFont($tulisan.val(), font);
+  }
+
+  function renderFont(writtenData, font){
+    asciiMau.write(writtenData, font, function(asciiArt){
+      $("#asciiArt").html("<pre>" + asciiArt + "</pre>")
+    });
   }
   
   /**
    * Mendengarkan events
    */
   $fontlistEl.change(function(){
-    asciiMau.loadFont($(this).val(), handleLoadedFont);
+    $(document).trigger("fontUpdated", {
+      name: $(this).val()
+    });
+    // asciiMau.loadFont($(this).val(), handleLoadedFont);
+  });
+
+  $tulisan.keyup(function(){
+    $(document).trigger("writtenTextUpdated", {
+      text: $(this).val()
+    });
+    // renderFont($(this).val());
   });
   
 
