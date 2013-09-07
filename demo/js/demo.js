5f399ef976001d753e7661429c82ed9e69d0c702
A. Akbar Hidayat <keripix@gmail.com>
2013-09-07 12:14:25 +0700

    capitalize

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -9,6 +9,7 @@
    * Listening to events
    */
   fontList.forEach(function(key){
+    key = key.charAt(0).toUpperCase() + key.substr(1);
     $fontlistEl.append("<option value='" + key + "'>" + key + "</option>")
   }, this);
 
