02b8e900310e617db17a0387c06cf4ee4ad82017
A. Akbar Hidayat <keripix@gmail.com>
2013-09-07 12:18:51 +0700

    font loaded

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -6,11 +6,26 @@
       $fontlistEl = $("#font-list");
 
   /**
-   * Listening to events
+   * Memuat fonts
    */
   fontList.forEach(function(key){
     key = key.charAt(0).toUpperCase() + key.substr(1);
     $fontlistEl.append("<option value='" + key + "'>" + key + "</option>")
   }, this);
+
+  /**
+   * Event handler yang akan kita gunakan
+   */
+  function handleLoadedFont(content){
+    console.log(content);
+  }
+  
+  /**
+   * Mendengarkan events
+   */
+  $fontlistEl.change(function(){
+    asciiMau.loadFont($(this).val(), handleLoadedFont);
+  });
+  
 
 })(this, $);
